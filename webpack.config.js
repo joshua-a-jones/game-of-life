6f37e5ec471d4504bb89225fd72a8474b644cdac
Alan Jones <aljones1816@gmail.com>
2022-04-01 16:41:55 -0400

    add typescript loader to webpack and mofify config

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,21 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.ts',
+  entry: "./src/index.ts",
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
   },
 };
