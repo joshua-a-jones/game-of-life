753993d41e652a1dde7f3127a6bb4b643f54b8b4
Joshua Jones <joshua.desjones@gmail.com>
2022-04-04 07:45:11 -0500

    updated classes to make more sense

--- src/gameLogic/Board.ts
+++ src/gameLogic/Board.ts
@@ -1,48 +1,11 @@
 class Board {
-    _dimensions: number;
-    _rows: Array<Row>;
+    private _dimensions: number;
+    private _grid: Grid<Cell>;
 
-    // constructor
-    Board(dimensions: number) {
+    // constructor for square board
+    public constructor(dimensions: number) {
         this._dimensions = dimensions;
-        this._rows = new Array<Row>(dimensions);
-    }
 
-    countLivingNeighborsOfSquareAt(x: number, y: number) {
-        let numberOfLivingNeighbors = 0;
-
-        // loop through all squares neighboring the square at x, y
-        for (let i = x - 1; i <= x + 1; i++) {
-            for (let j = y - 1; j <= y + 1; j++) {
-                numberOfLivingNeighbors += this._rows[j]
-                    .getSquare(i)
-                    ?.getStatus()
-                    ? 1
-                    : 0;
-            }
-        }
-
-        // if the specified square is alive, the above loop would have counted it, so need to remove
-        numberOfLivingNeighbors -= this._rows[y].getSquare(x).getStatus()
-            ? 1
-            : 0;
-
-        return numberOfLivingNeighbors;
-    }
-
-    updateSquareStatus(square: Square, numberOfLivingNeighbors: number) {
-        // code here for the rules of whether a square is alive or dead
-    }
-
-    updateStatusOfAllSquares() {
-        // loop through all squares and set status based on number of living neighbors.
-        for (let i = 0; i < this._dimensions; i++) {
-            for (let j = 0; j < this._dimensions; j++) {
-                const square = this._rows[j].getSquare(i);
-                const numberOfLivingNeighbors =
-                    this.countLivingNeighborsOfSquareAt(i, j);
-                this.updateSquareStatus(square, numberOfLivingNeighbors);
-            }
-        }
+        this._grid = new Grid<Cell>(dimensions, dimensions);
     }
 }
---
+++ src/gameLogic/Grid.ts
@@ -0,0 +1,22 @@
+class Grid<T> {
+    private _columns: number;
+    private _rows: number;
+    private _grid: Array<Array<T>>;
+
+    public constructor(columns: number, rows: number) {
+        this._columns = columns;
+        this._rows = rows;
+
+        let grid = new Array(columns);
+
+        for (let i = 0; i < columns; i++) {
+            grid[i] = new Array<T>(rows);
+        }
+    }
+
+    getElementAt(x: number, y: number): T | null {
+        return x > 0 && x < this._columns && y > 0 && y < this._rows
+            ? this._grid[x][y]
+            : null;
+    }
+}
