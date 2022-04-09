d773da71c5fe6bbb1ac0128fd02f9ef41e4a58a1
Alan <aljones1816@gmail.com>
2022-04-09 16:57:12 -0400

    add logic for grab and drag canvas

--- src/UI/components/board.ts
+++ src/UI/components/board.ts
@@ -1,36 +1,96 @@
 import style from './board.module.css';
 import Board from 'src/Game/Board';
 
-const boardContainer = document.createElement('div');
+interface BoardContainerProps {
+    Board: Board;
 
-// function that accepts a Board object and renders the grid to the canvas
-function renderCanvas(board: Board) {
+}
+
+function BoardContainer(props: BoardContainerProps) {
+    let { Board } =
+        props;
+
+    let initialX = 0;
+    let initialY = 0;
+    let offsetX = 0;
+    let offsetY = 0;
+
+    const boardContainer = document.createElement('div');
+    boardContainer.setAttribute('id', 'boardContainer');
+    boardContainer.classList.add(style.board);  
+
     const canvas = document.createElement('canvas');
-    // canvas width is going to become dynamic based on window size, but setting it to a fixed value for now
+    canvas.setAttribute('id', 'canvas');
     canvas.width = 1000;
     canvas.height = 1000;
     const ctx = canvas.getContext('2d');
     canvas.classList.add(style.canvas);
-    const cellSize = 50;
 
-    // currently setting each cell to 50 pixels wide and tall but we can adjust later
-    if (ctx) {
-        for (let i = 0; i < canvas.width / cellSize; i++) {
-            for (let j = 0; j < canvas.height / cellSize; j++) {
-                ctx.strokeRect(i * cellSize, j * cellSize, cellSize, cellSize);
-            }
-        }
-        board.getBoardState().forEach((cell) => {
-            ctx.fillStyle = '#f000f0';
-            ctx.fillRect(
-                cell.coordinates.x * cellSize,
-                cell.coordinates.y * cellSize,
-                cellSize,
-                cellSize
-            );
-        });
+    boardContainer.appendChild(canvas);
+
+    canvas.addEventListener('mousedown', (e) => {
+        e.preventDefault;
+        canvas.style.cursor = 'grabbing';
+        console.log(offsetX, offsetY);
+        initialX = e.clientX - canvas.getBoundingClientRect().left - offsetX;
+        initialY = e.clientY - canvas.getBoundingClientRect().top - offsetY;
+        document.addEventListener('mousemove', handleCoordinatesForRender);
+    });
+
+    document.addEventListener('mouseup', (e) => {
+        e.preventDefault;
+        canvas.style.cursor = 'default';
+      
+        document.removeEventListener('mousemove', handleCoordinatesForRender);
+    });
+
+    function handleCoordinatesForRender(e: MouseEvent) {
+
+        let canvasboundingrect = canvas.getBoundingClientRect();
+        offsetX = e.clientX - canvasboundingrect.left - initialX;
+        offsetY = e.clientY - canvasboundingrect.top - initialY;
+    
+        handleCanvasTransform(offsetX, offsetY);
+    
     }
-    return canvas;
+
+    function handleCanvasTransform(offsetX = 0, offsetY = 0) {
+        if (ctx) {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            handleRenderCanvas(50,offsetX,offsetY);
+        }
+        
+    }
+
+    function handleRenderCanvas(cellSize: number, offsetX: number, offsetY: number) {
+
+        if (ctx) {
+            for (let i = 0; i < canvas.width / cellSize; i++) {
+                for (let j = 0; j < canvas.height / cellSize; j++) {
+                    ctx.strokeRect(i * cellSize + offsetX, j * cellSize + offsetY, cellSize, cellSize);
+                }
+            }
+            Board.getBoardState().forEach((cell) => {
+                ctx.fillStyle = '#f000f0';
+                ctx.fillRect(
+                    offsetX + cell.coordinates.x*cellSize,
+                    offsetY + cell.coordinates.y*cellSize,
+                    cellSize,
+                    cellSize
+                );
+            });
+        }
+    }
+
+    return { boardContainer, handleRenderCanvas}
 }
 
-export { boardContainer, renderCanvas };
+
+
+
+
+// function that accepts a Board object and renders the grid to the canvas
+
+
+
+export { BoardContainer };
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { boardContainer, renderCanvas } from './UI/components/board';
+import { BoardContainer} from './UI/components/board';
 import { ControlsContainer } from './UI/components/controls';
 import './globalStyles.css';
 import Board from './Game/Board';
@@ -6,6 +6,11 @@ let isRunning = false;
 
 //test
 const board = new Board(20, 20);
+
+let initialY=0;
+let initialX=0;
+let offsetX = 0;
+let offsetY = 0;
 
 function handleButtonClickRun() {
     isRunning = !isRunning;
@@ -16,6 +21,7 @@ function handleButtonClickRandomize() {
     board.randomizeBoardState();
     renderApp();
 }
+
 
 const renderApp = () => {
     const app = document.getElementById('app');
@@ -33,7 +39,16 @@ const renderApp = () => {
                 isRunning,
             })
         );
-        app.appendChild(boardContainer.appendChild(renderCanvas(board)));
+        
+        let newBoardContainer = BoardContainer({ Board: board });
+        app.appendChild(newBoardContainer.boardContainer);
+        newBoardContainer.handleRenderCanvas(50, 0,0);
+
+      
+
+        
+
+
     } else {
         throw new Error('App element could not be found in index.html');
     }
