09da66e799fdf8f4d81701b7749dd5d290b7d588
Joshua Jones <joshua.desjones@gmail.com>
2022-04-28 21:33:54 -0500

    update game controller to accept iteration callbak

--- src/Game/GameController.ts
+++ src/Game/GameController.ts
@@ -4,6 +4,10 @@ interface GameControllerProps {
     rerenderBoard: (board: Board) => void;
     maxIterations: number;
     board: Board;
+    retrieveCurrentIteration: (
+        currentIteration: number,
+        maxIterations: number
+    ) => void;
 }
 
 class GameController {
@@ -12,11 +16,16 @@ class GameController {
     private _currentIteration = 0;
     private _board: Board;
     private _stopGame = false;
+    private _retrieveCurrentIteration: (
+        currentIteration: number,
+        maxIterations: number
+    ) => void;
 
     public constructor(props: GameControllerProps) {
         this._rerenderBoard = props.rerenderBoard;
         this._maxIterations = props.maxIterations;
         this._board = props.board;
+        this._retrieveCurrentIteration = props.retrieveCurrentIteration;
 
         //this._rerenderBoard(this._board);
     }
@@ -44,10 +53,18 @@ class GameController {
         // this._currentIteration++;
         board.updateBoardState();
         this._rerenderBoard(board);
+        this._retrieveCurrentIteration(
+            this._currentIteration,
+            this._maxIterations
+        );
     }
 
     public stopGame() {
         this._stopGame = true;
+    }
+
+    public get getMaxIterations() {
+        return this._maxIterations;
     }
 }
 
